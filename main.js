75556c0bb7ed745d45e7720773370dd9670e9ada
annaborisenkoit <126327737+annaborisenkoit@users.noreply.github.com>
2023-06-17 19:42:24 +0700

    init

---
+++ main.js
@@ -0,0 +1,8 @@
+let film = ''; //
+
+//Получить из DOM:
+const filmAddInputNode = document.querySelector('.js-add__input');
+const newFilmBtnNode = document.querySelector('.js-add__btn');
+const filmsNode = document.querySelector('.js-films');
+
+newFilmBtnNode.addEventListener('clıck', function (event) {});
